aaad3a94817df5b1aaaf5298d2ec2ce751a76016
marioelsen <mail@elsenmedia.com>
2018-06-11 15:29:56 +0200

    Recordy Type Kommentare

--- src/RecordType.java
+++ src/RecordType.java
@@ -1,5 +1,8 @@
 /**
  * Eine Enum-Klasse für Records-Konstanten: commit, write und begin of TA
+ * @link BEGINOFTA - Transakationsbeginn
+ * @link WRITE - Schreibvorgang
+ * @link COMMIT  - Commit
  */
 
 public enum RecordType {
